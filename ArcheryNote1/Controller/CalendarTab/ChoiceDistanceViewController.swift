ae670f77fc13419443c365d2e35f141c856b2a3e
ohshimatani <ohshimatani0326@gmail.com>
2020-08-09 02:39:41 +0900

    enable to select distance

---
+++ ArcheryNote1/Controller/CalendarTab/ChoiceDistanceViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChoiceDistanceViewController.swift
+//  ArcheryNote1
+//
+//  Created by 大島谷遼 on 2020/08/09.
+//  Copyright © 2020 Ohshimatani Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ChoiceDistanceViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+
+}
